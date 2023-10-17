32acc9805cb60a3c55e3236f396b300d1060bba0
André Coelho <andrealbergaria@gmail.com>
2023-10-17 05:15:20 +0100

    Added java bug

---
+++ javaZeroBug/javaBugZero.java
@@ -0,0 +1,19 @@
+﻿//BUG
+
+package test;
+
+public class zeroString {
+	
+	public static void main(String[] args) {
+		char[] cA = new char[3];
+		cA[0] = 'A';
+		cA[1] = 0;
+		cA[2] = 'B';
+		String str = new String(cA);
+		System.out.println(str);
+
+	}
+}
+
+
+//doesnt print 'B'
