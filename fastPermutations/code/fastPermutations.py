310994c13b55a4c02f8b02506adcaa378c7f0202
André Coelho <andrealbergaria@gmail.com>
2024-09-27 02:55:14 +0100

    Added cca

---
+++ fastPermutations/code/fastPermutations.py
@@ -0,0 +1,12 @@
+﻿def permutation(lst):
+    l = lst
+    result_list = []
+
+    for i in range(0,len(lst)):
+        l = l[1:] + l[:1]
+        result_list.append(l)
+    input(f" {result_list}")
+    return result_list
+
+#Driver code
+permutation([1,2,34,4,5,67,78])
